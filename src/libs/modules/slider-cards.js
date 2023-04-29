9c627a4b91a8e74d459b3fcb3819a8b70006f0f3
flashgrinder <flashgrinder92@gmail.com>
2023-04-29 12:11:38 +0400

    make page eco-education/slider-cards

--- src/libs/modules/slider-cards.js
+++ src/libs/modules/slider-cards.js
@@ -8,35 +8,49 @@ function init() {
 
     listSlider.forEach((item) => {
         let slider = item.querySelector('.js-slider-cards');
+        let sliderOneCard = item.querySelector('.js-slider-cards-one');
 
         const nextEl = item.querySelector('.js-slider-cards-next');
         const prevEl = item.querySelector('.js-slider-cards-prev');
 
-        let sliderCards = new Swiper(slider, {
-            slidesPerView: 1,
-            autoHeight: true,
-            loop: false,
-            navigation: {
-                nextEl: nextEl,
-                prevEl: prevEl
-            },
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                    spaceBetween: 5
+        if ( slider) {
+            let sliderCards = new Swiper(slider, {
+                slidesPerView: 1,
+                autoHeight: true,
+                loop: false,
+                navigation: {
+                    nextEl: nextEl,
+                    prevEl: prevEl
                 },
-                576: {
-                    slidesPerView: 2,
-                },
-                768: {
-                    slidesPerView: 3,
-                },
-                1024: {
-                    slidesPerView: 4,
-                    slidesPerColumn: 1
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 5
+                    },
+                    576: {
+                        slidesPerView: 2,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        slidesPerColumn: 1
+                    }
                 }
-            }
-        });
+            });
+        }
+        if ( sliderOneCard) {
+            let sliderCardsOne = new Swiper(sliderOneCard, {
+                slidesPerView: 1,
+                autoHeight: true,
+                loop: true,
+                navigation: {
+                    nextEl: nextEl,
+                    prevEl: prevEl
+                },
+            });
+        }
 
     })
 
